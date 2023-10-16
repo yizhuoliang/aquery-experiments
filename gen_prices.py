311627f740b34f2eaba828241df5770b3c3770df
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2023-10-16 10:11:24 -0400

    add gen prices script

---
+++ gen_prices.py
@@ -0,0 +1,21 @@
+import csv
+import random
+
+# Number of lines
+num_lines = 100000
+
+# Generate months in random order
+months = list(range(1, num_lines + 1))
+random.shuffle(months)
+
+# Generate random prices
+prices = [random.randint(1, 2000) for _ in range(num_lines)]
+
+# Write to CSV file
+with open('prices.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['month', 'price'])  # Write header
+    for month, price in zip(months, prices):
+        csvwriter.writerow([month, price])
+
+print(f"CSV file with {num_lines} lines generated as 'output.csv'")
